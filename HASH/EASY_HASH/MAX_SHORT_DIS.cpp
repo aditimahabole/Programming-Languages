5eb4347c800f69d0e2101adc91ece84d90297e84
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2022-01-08 15:51:39 +0530

    Create MAX_SHORT_DIS.cpp

---
+++ HASH/EASY_HASH/MAX_SHORT_DIS.cpp
@@ -0,0 +1,43 @@
+
+// https://www.geeksforgeeks.org/print-maximum-shortest-distance/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+bool sum_short_end(int arr[],int n,int k)
+{
+    unordered_map<int,int> lo;
+    for(int i=0;i<n;i++)
+    {
+        int x=arr[i];
+        int d=min(i+1,n-i);
+        if(lo.find(x)==lo.end())
+        {
+            lo[x]=d;
+        }
+        else
+        {
+            //if duplicate fount then chck for min dis from ends
+            lo[x]=min(d,lo[x]);
+        }
+    }
+    int ans=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        int x=arr[i];
+        if(x!=k-x &&lo.find(k-x)!=lo.end())
+        {
+            ans=min(max(lo[x],lo[k - x]),ans);
+        }
+    }
+    return ans;
+}
+int main() 
+{
+    int arr[] = {2, 4, 3, 2, 1};
+    int k=5;
+    int n = sizeof(arr) / sizeof(int);
+    cout<<sum_short_end(arr,n,k)<<endl;
+    return 0;
+}
+//OUTPUT : 1
