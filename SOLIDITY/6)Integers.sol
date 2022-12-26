3326b7e8d35ed514fe854a5dd3c7e62d443e4de8
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2022-12-26 14:43:13 +0530

    Create 6)Integers.sol

---
+++ SOLIDITY/6)Integers.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+contract Integers
+{
+//    int->- +
+//    uint ->only + 
+//    by default value is 0 
+//    range of int8 
+//    -128 to +127
+//    uint8 0 to 255
+
+
+}
