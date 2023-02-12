4827485f6270410620ae98781e2750606c53c40a
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2023-02-12 13:26:52 +0530

    Create update_object_state.js

---
+++ REACT/update_object_state.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    
+    let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png" 
+//     this will help to show colored or empty star
+    
+    function toggleFavorite() {
+        setContact(state => {
+            return(
+               { ...state, 
+//                 always use spread operator else we are making new object in which only isFav property is present this is s bug
+                isFavorite: !state.isFavorite
+               }
+            )    
+        }
+        )
+    }
+    return (
+        <main>
+            <article className="card">
+                <img src="./images/user.png" className="card--image" />
+                <div className="card--info">
+                    <img 
+                        src={`../images/${starIcon}`}  
+//                         rendering star icon here
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+                
+            </article>
+        </main>
+    )
+}
