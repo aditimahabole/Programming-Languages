d011f53a8bfe7094ffade0d12a9a919568eb14e3
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2023-02-12 14:57:50 +0530

    Create Changing_box_color_declarative.js

---
+++ REACT/Changing_box_color_declarative.js
@@ -0,0 +1,29 @@
+import React from "react"
+import boxes from "./boxes"
+import Box from "./Box"
+
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    function toggle(id) {
+        setSquares(prevSquares => {
+            return prevSquares.map((square) => {
+                return square.id === id ? {...square, on: !square.on} : square
+            })
+        })
+    }
+    const squareElements = squares.map(square => (
+        <Box 
+            key={square.id} 
+            id={square.id}
+            on={square.on} 
+            toggle={toggle}
+        />
+    ))
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
