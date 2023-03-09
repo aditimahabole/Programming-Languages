6929d20b5370f1824badfccd8b7eac07aeab2fee
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2023-03-09 10:50:49 +0530

    Update GENETIC_ALGO.py

--- AI_LAB/GENETIC_ALGO.py
+++ AI_LAB/GENETIC_ALGO.py
@@ -1,61 +1,96 @@
-# __https://youtu.be/4XZoVQOt-0I__
-import random 
-import math
-def fun(x,y,z):
-    return x+y+z -25
-def fitness(x,y,z):
-    ans = fun(x,y,z)
-    # ranking
-    if(ans == 0):
-        return 99 #very good
-    else: 
-        return abs(1/ans)
-        # return math.floor(abs(1/ans))
-        #smaller the denominator bigger the result
-        #we will get a high number when sorting and higer is better
-#generate solutions 
-sol = []
-for s in range(50):
-    # sol.append((random.uniform(0,10000),random.uniform(0,10000),random.uniform(0,10000)))
-    sol.append((random.randint(0,50),random.randint(0,50),random.randint(0,50)))
-print("----1st 5 solutions are  ---- ")
-for i in range(0,5):
-    print(sol[i])
-print("----Ranked Solutions ---- ")
-for i in range(10):
-    ranked_sols = []
-    for s in sol:
-        ranked_sols.append((fitness(s[0],s[1],s[2]),s))
-    ranked_sols.sort()
-    ranked_sols.reverse()
-    print(f"=== Gen {i} best solutions ===")
-    for m in range(0,3):
-        print("%.2f" %ranked_sols[m][0])
-        print(ranked_sols[m][1])
-        print("-------------------------")
-        
-    if ranked_sols[0][0]>99:
-        break
-    
-    best_sols = ranked_sols[0:5]
-    elements = []
-    for ele in best_sols:
-        elements.append(ele[1][0])
-        elements.append(ele[1][1])
-        elements.append(ele[1][2])
-    new_gen = []
-    for i in range(100):
-       e1 = random.choice(elements) *random.uniform(0.99,1.01)
-       e2 = random.choice(elements) *random.uniform(0.99,1.01)
-       e3 = random.choice(elements) *random.uniform(0.99,1.01)
-       
-       new_gen.append((e1,e2,e3))
-    sol = new_gen
-    
-        
-        
-    
-    
-        
-    
-    
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+// ----------------------------------------------
+struct Solution 
+{
+    double rank,x,y,z;
+    void fitness()
+    {
+        double ans = (6*x +  -y +pow(z,200)) - 25;
+        ans == 0 ? rank = 9999: rank = abs(1/ans);
+        // cout<<"Rank Calculated : "<<rank<<endl;
+        // more value lead to zero
+    }
+};
+// ----------------------------------------------
+int main()
+{
+    //----------------------------------------------------
+    // create random solutions 
+    random_device device;
+    uniform_real_distribution<double> uniform(-100,100);
+    vector<Solution> solutions;
+    // 100000
+    const int NUM = 10000;
+    for( int i = 0;i<NUM;i++)
+    solutions.push_back(Solution{0,
+    uniform(device), //x
+    uniform(device), //y
+    uniform(device)  //z
+    //values initialized between -10000 to 10000
+    });
+    //----------------------------------------------------
+    while(true){
+    // run fitness function
+    cout<<"------Calculation Fitness------"<<endl;
+    for(auto &s:solutions) s.fitness();
+    //----------------------------------------------------
+    //sorting based on fitness
+    sort(solutions.begin(),solutions.end(),
+    [](const auto& lhs,const auto&rhs)
+    {
+        return lhs.rank >rhs.rank;
+    }
+    );
+    //----------------------------------------------------
+    // print top solutions
+    cout<<"------Top Solutions------"<<endl;
+    for(int i=0;i<=5;i++)
+    {
+        cout<<"Rank : "<<solutions[i].rank<<endl;
+        cout<<"x : "<<solutions[i].x<<endl;
+        cout<<"y : "<<solutions[i].y<<endl;
+        cout<<"z : "<<solutions[i].z<<endl;
+    }
+    //----------------------------------------------------
+    //Take top solutions
+    const int SAMPLE_SIZE = 50;
+    vector<Solution>sample;
+    // copy top 1000 solutions to sample
+    for(int i=0;i<SAMPLE_SIZE;i++)
+    {
+        sample.push_back(solutions[i]);
+    }
+    // discarding other population
+    solutions.clear();
+    //----------------------------------------------------
+    // Mutate top solutions
+    uniform_real_distribution<double>m(0.99,1.01);
+    // mutate by atmost 1 percent
+    cout<<"------Doing Mutation------"<<endl;
+    for(int i=0;i<sample.size();i++)
+    {
+        sample[i].x *= m(device);
+        sample[i].y *= m(device);
+        sample[i].z *= m(device);
+        // changing values by 1 percent
+    }
+    //----------------------------------------------------
+    //Cross Over 
+    uniform_int_distribution<int> cross(0,SAMPLE_SIZE - 1);
+    cout<<"------Doing CrossOver------"<<endl;
+    for(int i=0;i<NUM;i++)
+    {
+        solutions.push_back(Solution{
+            0,
+            sample[cross(device)].x,
+            sample[cross(device)].y,
+            sample[cross(device)].z
+        });
+    }
+    }
+    cout<<"yay"<<endl;
+    return 0;
+}
