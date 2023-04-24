1c228238603cf63505058d79f17e1f3079f465ab
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2023-04-24 21:06:46 +0530

    Create 1.Intro_RUST.rs

---
+++ RUST/1.Intro_RUST.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    println!("hey");
+    let num = 7; //this is immutable
+    let mut num1 = 5; // this is mutable (can be changed)
+    println!("number : {num}");
+    num = 8;
+    println!("number new : {num}");
+}
