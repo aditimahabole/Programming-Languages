222b9b3e126b3ff265ad9df791d397ffa151ed49
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2023-04-26 17:01:52 +0530

    Create 6.Print.rs

---
+++ RUST/6.Print.rs
@@ -0,0 +1,12 @@
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    //this -> arrow returns a type
+
+    msg!("Hello Buddy"); // msg is used to print
+    Ok(())
+    
+}
